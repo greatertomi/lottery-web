a9ce3eb7a156de3c79627bb0b13c3ed5f6b3f13d
John Oshalusi <john.oshalusi@iohk.io>
2022-05-25 22:43:39 +0100

    app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import lottery from "./lottery";
 
-function App() {
+const App = () => {
+  const [manager, setManager] = useState('');
+
+  useEffect(async () => {
+    const currentManager = await lottery.methods.manager().call();
+    setManager(currentManager);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager}</p>
     </div>
-  );
+  )
 }
 
 export default App;
